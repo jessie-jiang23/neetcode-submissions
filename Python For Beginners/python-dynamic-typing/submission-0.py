fba003c4c2a2734fd8a56678fa69a826470b4301
Jessie Jiang <jiahej91@gmail.com>
2026-05-29 21:55:52 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hi"
+print(type(variable))
+variable = [1,2,3,4]
+print(type(variable))
